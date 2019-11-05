fc15656730c1903f99bb432d64750014fd000713
Titus Ntandika <mcewentito@gmail.com>
2019-11-05 22:25:39 +0200

    Changed theme, Added hint text

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,6 +6,7 @@ import { Storage } from '@ionic/storage';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
 export class Tab3Page {
   
   showFlipCameraButton : boolean;
@@ -16,19 +17,12 @@ export class Tab3Page {
   
   constructor(private settings: Storage) {}
 
-  toggleShowTorchButton(event: any){
-    console.log(event);
-    // this.showTorchButton = event;
-    this.settings.set('showTorchButton', event);
-  }
-
   toggleSetting(setting: string, event: any){
     console.log("setting : " + event);
     this.settings.set(setting, event);
-    this.settings.get(setting).then((value)=>{
-      console.log("Setting: " + setting + " " + value);
+    this.settings.get(setting)
+      .then((value)=>{
+        console.log("Setting: " + setting + " " + value);
     })
   }
-
-
 }
